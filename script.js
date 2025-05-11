9c2badd1a9ebb5a035792ba2cdeee87e300e09ec
OhKh09373 <202209373@stu.uob.edu.bh>
2025-05-11 16:02:48 +0300

    the button function

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 let home = document.querySelector(".SideButton#home");
+let edit = document.querySelector(".SideButton#edit");
+let add = document.querySelector(".SideButton#add");
+let remove = document.querySelector(".SideButton#delete");
 let homeContent = document.querySelector(".Home-content");
+let editContent = document.querySelector(".Edit-content");
+let AddContent = document.querySelector(".Add-content");
+let RemoveContent = document.querySelector(".Delete-content");
 
 home.addEventListener("click", showHome);
+edit.addEventListener("click", showEdit);
+add.addEventListener("click", showAdd);
+remove.addEventListener("click", showRemove);
 
 function showHome() {
-    console.log("Home button clicked");
+    homeContent.style.display ="flex";
+    editContent.style.display = "none";
+    AddContent.style.display = "none";
+    RemoveContent.style.display = "none";
+}
 
-    if (homeContent) {
-        const currentDisplay = getComputedStyle(homeContent).display;
+function showEdit() {
+    homeContent.style.display ="none";
+    editContent.style.display = "flex";
+    AddContent.style.display = "none";
+    RemoveContent.style.display = "none";
+}
 
-        homeContent.style.display = (currentDisplay === "none") ? "flex" : "none";
-    }
+function showAdd() {
+    homeContent.style.display ="none";
+    editContent.style.display = "none";
+    AddContent.style.display = "flex";
+    RemoveContent.style.display = "none";
+}
+
+function showRemove() {
+    homeContent.style.display ="none";
+    editContent.style.display = "none";
+    AddContent.style.display = "none";
+    RemoveContent.style.display = "flex";
 }
