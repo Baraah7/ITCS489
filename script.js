ab31fb8a04d7c8098cc1111f23aa4e225e6a39f1
OhKh09373 <202209373@stu.uob.edu.bh>
2025-05-14 18:32:17 +0300

    Bug fix

--- script.js
+++ script.js
@@ -2,51 +2,31 @@ let home = document.querySelector(".SideButton#home");
 let edit = document.querySelector(".SideButton#edit");
 let add = document.querySelector(".SideButton#add");
 let remove = document.querySelector(".SideButton#delete");
+
 let homeContent = document.querySelector(".Home-content");
 let editContent = document.querySelector(".Edit-content");
 let AddContent = document.querySelector(".Add-content");
 let RemoveContent = document.querySelector(".Delete-content");
 
-home.addEventListener("click", showHome);
-edit.addEventListener("click", showEdit);
-add.addEventListener("click", showAdd);
-remove.addEventListener("click", showRemove);
+// Adding event listeners
+home.addEventListener("click", () => showSection(homeContent));
+edit.addEventListener("click", () => showSection(editContent));
+add.addEventListener("click", () => showSection(AddContent));
+remove.addEventListener("click", () => showSection(RemoveContent));
 
-function showHome() {
-    homeContent.style.display ="flex";
-    editContent.style.display = "none";
-    AddContent.style.display = "none";
-    RemoveContent.style.display = "none";
-}
+function showSection(sectionToShow) {
+    // Hide all sections
+    const allSections = [homeContent, editContent, AddContent, RemoveContent];
+    allSections.forEach(section => section.style.display = "none");
 
-function showEdit() {
-    homeContent.style.display ="none";
-    editContent.style.display = "flex";
-    AddContent.style.display = "none";
-    RemoveContent.style.display = "none";
-}
-
-function showAdd() {
-    homeContent.style.display ="none";
-    editContent.style.display = "none";
-    AddContent.style.display = "flex";
-    RemoveContent.style.display = "none";
-}
-
-function showRemove() {
-    homeContent.style.display ="none";
-    editContent.style.display = "none";
-    AddContent.style.display = "none";
-    RemoveContent.style.display = "flex";
+    // Show the selected section
+    sectionToShow.style.display = "flex";
 }
 
 let editbutton = document.getElementById("find-edit");
-let removebutton = document.querySelector("#find-delete");    
-let inputfrom = document.querySelector("#input-form");
 let editform = document.querySelector("#edit-form");
 
 editbutton.addEventListener("click", findbook);
-removebutton.addEventListener("click", findbook);
 
 function findbook() {
     editform.style.display = "flex";
