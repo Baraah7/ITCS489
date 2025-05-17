a787fb43ebace3f7d8d967f8604f6960a9042641
Ok <202209373@stu.uob.edu.bh>
2025-05-18 01:58:31 +0300

    Create Login.js

---
+++ public/js/Login.js
@@ -0,0 +1,19 @@
+let user = document.querySelector("#username");
+let password = document.querySelector("#password");
+
+let Button = document.getElementById("loginButton");
+let errorMessage = document.querySelector("#errorMessage");
+
+Button.addEventListener("click", Login);
+
+function Login() {
+  let userValue = user.value;
+  let passwordValue = password.value;
+
+  if (userValue === "admin" && passwordValue === "admin") {
+    window.location.href = "home.html";
+  } else {
+    errorMessage.textContent = "Invalid username or password";
+    errorMessage.style.color = "red";
+  }
+}
